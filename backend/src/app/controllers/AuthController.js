9326f89f721806d72ebc7ae5000281c558dcb697
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-05-31 23:26:28 -0300

    fix: user_profile associate

--- backend/src/app/controllers/AuthController.js
+++ backend/src/app/controllers/AuthController.js
@@ -2,11 +2,11 @@ const { UserAccount } = require('../models');
 
 module.exports = {
   async store(req, res) {
-    const { password, profile_id } = req.body;
+    const { password, user_profile } = req.body;
 
     const UserAcc = await UserAccount.create({
       password,
-      profile_id,
+      user_profile,
     });
 
     return res.json(UserAcc);
